a144d314e2bbb0e556070f6124568be2603d16c5
Felipe Rafael Barros <gitfelipebarros@gmail.com>
2025-07-25 11:51:39 -0300

    - Entendendo Export e Import com JavaScript

---
+++ 4. JavaScript/Arq2.js
@@ -0,0 +1,12 @@
+//Importação e Exportação com JavaScript
+
+const funcoes = require('./funcoes-auxiliares');
+
+console.log(funcoes);
+
+console.log(funcoes.gets);
+
+// Objeto Destructuring
+
+// const { gets, prints } = require('./funcoes-auxiliares');
+
